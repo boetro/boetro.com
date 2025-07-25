98536eccced319ba182d8f1accef712e95a6377e
Caleb Van Dyke <ctvandyke24@gmail.com>
2025-07-24 23:59:38 -0500

    hide projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,12 +5,4 @@ export interface Project {
   githubLink?: string; // Made optional with '?'
 }
 
-export const projects: Project[] = [
-  {
-    slug: "smart-calendar",
-    title: "Smart Calendar",
-    description:
-      "A smart home calendar that makes it easy to view everyone in your homes schedule, as well as any other important information such as: grocery lists, weekly meal plans, etc..",
-    // githubLink: "https://github.com/boetro/smart-calendar",
-  },
-];
+export const projects: Project[] = [];
